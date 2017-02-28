905b3bb6efe52f4b27eecc02698daecf1afd215f
Sandermfc <mathieu.robinson17@gmail.com>
2017-02-28 18:31:17 -0400

    update

--- formatData.py
+++ formatData.py
@@ -1,8 +1,11 @@
 import os
 import json
 import re #regular expressions
+import datetime
 
 nGramDict = {};
+nGramVal = {};
+
 keyword = {};
 description = {};
 cmc = {};
@@ -11,7 +14,7 @@ dictPower = {};
 dictToughness = {};
 dictRarity = {};
 numOfReprints = {};
-
+releaseDateDict = {}
 
 
 def main():
@@ -23,6 +26,8 @@ def main():
 	setCode = {};
 	for set in data:
 		setCode[data[set]["name"].encode('utf-8')] = set;
+	with open("setCodes.json", 'w') as fp:
+		json.dump(setCode, fp);
 
 	#for each file which has price data
 	for fileName in os.listdir('price-by-set'):
@@ -33,11 +38,15 @@ def main():
 		#substring the fileName to get setName
 		pos = fileName.find('_');
 		setName = fileName[pos+1:len(fileName)-5];
-		print("Doing set "+setName);
+		print(setName);
 		#if setCode exists in bigFile
 		if setCode[setName] in data:
-
-			
+			if "releaseDate" in data[setCode[setName]]:
+				releaseDate = data[setCode[setName]]["releaseDate"].encode('utf-8');
+				releaseDateInt = int(releaseDate[:4])*365 + int(releaseDate[5:7])*30 + int(releaseDate[9:]); #int value of date = year*365 + month*30 + day
+			else:
+				#Something is wrong, shouldnt go here
+				releaseDateInt = 0;
 			#############################################################
 			###DO STUFF TO SCRAPE INFO FROM THE SET HERE (if you want)###
 			#############################################################
@@ -48,6 +57,7 @@ def main():
 			tempToughness = {};
 			tempRarity = {};
 			tempNumberOfPrintings = {};
+			tempReleaseDate = {};
 			#for each card in the set
 			for card in data[setCode[setName]]["cards"]:
 				###########
@@ -55,9 +65,7 @@ def main():
 				###########
 				cardName = card["name"].encode('utf-8');
 				price = getCardPrice(cardName, cardPrices);
-				print(price);
 				if(price > 0):
-					#print(price);
 					########################################################
 					###DO STUFF TO SCRAPE INFO FROM INDIVIDUAL CARDS HERE###
 					########################################################
@@ -66,55 +74,59 @@ def main():
 					#################
 					if "originalText" in card:
 						tempDescription[cardName.encode('utf-8')] = getDescription(card["name"].encode('utf-8'), price, card["originalText"].encode('utf-8'), setName);
-					#else:
-					#	print("NO ORIGINAL TEXT, GIVING VALUE OF 0");
+					else:
+						tempDescription[cardName] = "";
 					########################
 					###CONVERTED MANACOST###
 					########################
 					if "cmc" in card:
 						#getCMC(cardName, card["cmc"]);
 						tempCmc[cardName.encode('utf-8')] = card["cmc"];
-					#else:
-					#	print("NO CONVERTED MANACOST, GIVING CMC OF 0");
+					else:
+						tempCmc[cardName.encode('utf-8')] = 0;
 					##############
 					###MANACOST###
 					##############	
 					if "colorIdentity" in card:
 						tempNumOfColors[cardName] = len(card["colorIdentity"]);
 						#getNumOfColors(cardName, card["colorIdentity"]);
-					#else:
-					#	print("NO MANACOST FOUND");
+					else:
+						tempNumOfColors[cardName] = 0;
 					###########
 					###POWER###
 					###########
 					if "power" in card:
 						tempPower[cardName] = card["power"];
 						#getPower(cardName, card["power"]);
-					#else:
-					#	print("NO POWER");
+					else:
+						tempPower[cardName] = 0;
 					###############
 					###TOUGHNESS###
 					###############
 					if "toughness" in card:
 						tempToughness[cardName] = card["toughness"];
 						#getToughness(cardName, card["toughness"]);
-					#else:
-					#	print("NO TOUGHNESS");
+					else:
+						tempToughness[cardName] = 0;
 					############
 					###RARITY###
 					############
 					if "rarity" in card:
 						tempRarity[cardName] = getRarity(card["rarity"]);
-					#else:
-					#	print("NO RARITY");
+					else:
+						tempRarity[cardName]
 					########################
 					###NUMBER OF REPRINTS###
 					########################
 					if "printings" in card:
 						tempNumberOfPrintings[cardName] = len(card["printings"]);
 						#getNumberOfPrintings(cardName, card["printings"]);
-					#else:
-					#	print("NO PRINTINGS/NO REPRINTS?");
+					else:
+						tempNumberOfPrintings[cardName] = 1;
+					##################
+					###RELEASE DATE###
+					##################
+					tempReleaseDate[cardName] = releaseDateInt;
 				#else:
 				#	if(price == -1):
 				#		print(card["name"].encode('utf-8') + " was not found in " + fileName);
@@ -130,37 +142,54 @@ def main():
 			dictToughness[setCode[setName]] = tempToughness;
 			dictRarity[setCode[setName]] = tempRarity;
 			numOfReprints[setCode[setName]] = tempNumberOfPrintings;
+			releaseDateDict[setCode[setName]] = tempReleaseDate;
 		else:
 			print("ERROR, "+setCode+" does not exist in bigFile");
 
-	print("nGramDict size = "+str(len(nGramDict)));
-	print("cmc = "+str(len(cmc)));
-
-	with open("ngramCount"+".json", 'w') as fp:
+	#Heavily weighted average
+	for ngram in nGramDict:
+		minOccurences = 5;
+		priceList = [];
+		if(len(nGramDict[ngram]) >= minOccurences):
+			for price in nGramDict[ngram]:
+				priceList.append(price);
+			priceList.sort();
+			val=0;
+			for k in range(0, len(priceList)):
+				val = val + float(priceList[k].encode('utf-8'));
+			val/=len(priceList);
+			print(ngram + " --> "+str(val));
+			nGramVal[ngram] = val;
+			
+	print("parsedData/ammount of ngram's = "+str(len(nGramVal)));
+	with open("parsedData/ngramCount"+".json", 'w') as fp:
 		json.dump(nGramDict, fp);
-	with open("keywords"+".json", 'w') as fp:
+	with open("parsedData/keywords"+".json", 'w') as fp:
 		json.dump(keyword, fp);
-	with open("descriptions"+".json", 'w') as fp:
+	with open("parsedData/descriptions"+".json", 'w') as fp:
 		json.dump(description, fp);
-	with open("cmc"+".json", 'w') as fp:
+	with open("parsedData/cmc"+".json", 'w') as fp:
 		json.dump(cmc, fp);
-	with open("numOfColors"+".json", 'w') as fp:
+	with open("parsedData/numOfColors"+".json", 'w') as fp:
 		json.dump(numOfColors, fp);
-	with open("power"+".json", 'w') as fp:
+	with open("parsedData/power"+".json", 'w') as fp:
 		json.dump(dictPower, fp);
-	with open("toughness"+".json", 'w') as fp:
+	with open("parsedData/toughness"+".json", 'w') as fp:
 		json.dump(dictToughness, fp);
-	with open("rarity"+".json", 'w') as fp:
+	with open("parsedData/rarity"+".json", 'w') as fp:
 		json.dump(dictRarity, fp);
-	with open("numOfReprints"+".json", 'w') as fp:
+	with open("parsedData/numOfReprints"+".json", 'w') as fp:
 		json.dump(numOfReprints, fp);
+	with open("parsedData/releaseDate"+".json",'w') as fp:
+		json.dump(releaseDateDict, fp);
+
 
 def getCardPrice(cardName, cardPrices):
 	#-1 for "card not found"
 	#-2 for value = 0;
 	if cardName in cardPrices:
-		if(cardPrices[cardName][0] != 0):
-			return cardPrices[cardName][0][1:];
+		if(cardPrices[cardName][0][1:] != 0):
+			return cardPrices[cardName][0][1:].replace(',','');
 		else:
 			return -2;
 	elif cardName + " (1)" in cardPrices: #check for syntax of alternate artworks
@@ -168,7 +197,7 @@ def getCardPrice(cardName, cardPrices):
 		finalPrice = 0;
 		i = 1;
 		while((cardName + "(" + str(i) + ")") in cardPrices):
-			price = cardPrices[cardName + "(" + str(i) + ")"][0];
+			price = cardPrices[cardName + "(" + str(i) + ")"][0][1:];
 			if price != "" and float(price) != 0:
 				tot+=1;
 				finalPrice += price;
@@ -178,13 +207,14 @@ def getCardPrice(cardName, cardPrices):
 		else:
 			return -2;
 		#print(finalPrice);
-		return finalPrice[1:];
+		return finalPrice[1:].replace(',','');
 	else:
 		return -1;
 
 def getDescription(cardName, cardPrice, originalText, setName):
-	originalText = originalText.replace(cardName, "this").lower();
+	originalText = originalText.replace(cardName, "this");
 	#originalText = jeansFunction();
+	originalText = originalText.lower();
 	getNGramCount(cardName,cardPrice,originalText, setName);
 	return originalText;
 
@@ -195,7 +225,7 @@ def getNGramCount(cardName, cardPrice, originalText, setName):
 		numWords = numGram;
 		for x in range(0, len(sent)):
 			temp+=sent[x]+" ";
-			nGramDict.setdefault(temp[:-1],[]).append(cardPrice);
+			nGramDict.setdefault(temp.strip(),[]).append(cardPrice);
 			numWords-=1;
 			if(numWords==0):
 				break;
@@ -232,7 +262,7 @@ def getNGramCount(cardName, cardPrice, originalText, setName):
 			temp="";
 			for k in range(0,numGram): #get the next 3 words
 				temp+=sentences[i][j+k]+" ";
-			nGramDict.setdefault(temp[:-1],[]).append(cardPrice);
+			nGramDict.setdefault(temp.strip(),[]).append(cardPrice);
 
 def getCMC(cardName, cmcVal):
 	cmc[cardName.encode('utf-8')] = cmcVal;
@@ -251,15 +281,19 @@ def getToughness(cardName, toughnessVal):
 
 def getRarity(rarityVal):
 	if(rarityVal == "Common"):
-		return 2;
+		#print("Common");
+		return "Common";
 	elif(rarityVal == "Uncommon"):
-		return 4;
+		#print("Uncommon");
+		return "Uncommon";
 	elif(rarityVal == "Rare"):
-		return 8;
+		#print("Rare");
+		return "Rare";
 	elif(rarityVal == "Mythic Rare"):
-		return 16;
+		#print("Mythic Rare");
+		return "Mythic Rare";
 	else:
-		return 1;
+		return "Nothing";
 	#print("rarity "+str(dictRarity[cardName.encode('utf-8')]));
 
 def getNumberOfPrintings(cardName, printings):
